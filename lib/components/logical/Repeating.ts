8ca8182ac43c99cf39e59e3361bf2c0212f78862
Felix Wotschofsky <hello@felisk.io>
2019-11-16 16:16:33 +0100

    Added "Repeating" Logical Component

---
+++ lib/components/logical/Repeating.ts
@@ -0,0 +1,25 @@
+import Component from '../../Component'
+import RenderUtils from '../../utils/Render'
+import PropsContext from '../../PropsContext'
+import Coordinates from '../../helpers/Coordinates'
+
+
+export type RepeatingProps = {
+   list: any[],
+   component: Component<any>,
+   position: (data: any) => Coordinates,
+   props: (data: any) => {}
+}
+
+export default class Repeating extends Component<RepeatingProps> {
+   public render(context: RenderingContext, position: Coordinates, props: RepeatingProps): void {
+      props.list.forEach((data) => {
+         const propsContext = new PropsContext(props.props(data))
+         RenderUtils.renderTemplateItem({
+            component: new props.component,
+            position: () => props.position(data),
+            props: () => props.props(data),
+         }, context, position, propsContext)
+      })
+   }
+}
