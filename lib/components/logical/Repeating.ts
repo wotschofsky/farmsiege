5d7dd46b476e53a9f0a6de89d19c1c66c2cd2a2a
Felix Wotschofsky <hello@felisk.io>
2019-11-17 10:06:11 +0100

    Added index to resolver functions for "Repeating"

--- lib/components/logical/Repeating.ts
+++ lib/components/logical/Repeating.ts
@@ -7,18 +7,18 @@ import Coordinates from '../../helpers/Coordinates'
 export type RepeatingProps = {
    list: any[],
    component: Component<any>,
-   position: (data: any) => Coordinates,
-   props: (data: any) => {}
+   position: (data: any, index: number) => Coordinates,
+   props: (data: any, index: number) => {}
 }
 
 export default class Repeating extends Component<RepeatingProps> {
    public render(context: RenderingContext, position: Coordinates, props: RepeatingProps): void {
-      props.list.forEach((data) => {
+      props.list.forEach((data, index) => {
          const propsContext = new PropsContext(props.props(data))
          RenderUtils.renderTemplateItem({
             component: props.component,
-            position: () => props.position(data),
-            props: () => props.props(data),
+            position: () => props.position(data, index),
+            props: () => props.props(data, index),
          }, context, position, propsContext)
       })
    }
