a6a0772c50e0a401a1e3285094ab99a7c3c7c8bd
Felix Wotschofsky <hello@felisk.io>
2019-11-21 19:26:52 +0100

    Made rabbits stop and start moving again

--- src/store/MovablesStore.ts
+++ src/store/MovablesStore.ts
@@ -10,6 +10,7 @@ export type RabbitData = {
    y: number,
    direction: Directions,
    movingTimeLeft: number,
+   timeBeforeMove: number,
 }
 
 type MovablesStoreContent = {
@@ -32,12 +33,24 @@ export default class MovablesStore extends Store<MovablesStoreContent> {
          clonedState.rabbits = clonedState.rabbits.map((data): RabbitData => {
             const speed = data.direction === Directions.Left ? 0.2 : -0.2
             let x = data.movingTimeLeft > 0 ? data.x - timeDifference * speed : data.x
+
+            let timeBeforeMove = data.timeBeforeMove
+            if(data.movingTimeLeft === 0) {
+               timeBeforeMove = Math.max(data.timeBeforeMove - timeDifference, 0)
+            }
+
             let movingTimeLeft = Math.max(data.movingTimeLeft - timeDifference, 0)
+            if(data.timeBeforeMove === 0) {
+               movingTimeLeft = Math.random() * 1500 + 3000
+               timeBeforeMove = Math.random() * 2000 + 1000
+            }
+
 
             return {
                ...data,
                x,
-               movingTimeLeft
+               movingTimeLeft,
+               timeBeforeMove
             }
          })
 
@@ -60,13 +73,14 @@ export default class MovablesStore extends Store<MovablesStoreContent> {
          }
 
          const direction = Math.random() > 0.5 ? Directions.Left : Directions.Right
-         const mappedRabbits: RabbitData[] = rabbitRows.map((row) => {
+         const mappedRabbits: RabbitData[] = rabbitRows.map((row): RabbitData => {
             const offset = Math.random() * 256
             return {
                x: direction === Directions.Left ? 1600 + offset : -128 - offset,
                y: row * 128 - 96,
                direction: direction,
                movingTimeLeft: Math.random() * 1500 + 3000,
+               timeBeforeMove: Math.random() * 2000 + 1000
             }
          })
 
