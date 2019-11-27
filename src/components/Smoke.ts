a97bc32f8c9860a7774cfe17f9cc8be972de597c
Felix Wotschofsky <hello@felisk.io>
2019-11-27 22:07:54 +0100

    Added smoke

---
+++ src/components/Smoke.ts
@@ -0,0 +1,29 @@
+import { Template } from '../../lib/Types'
+import AnimatedSprite, { AnimatedSpriteProps } from '../../lib/components/native/AnimatedSprite'
+import Component from '../../lib/Component'
+import Coordinates from '../../lib/helpers/Coordinates'
+import PropsContext from '../../lib/PropsContext'
+
+import smokeSprites from '../assets/smoke.png'
+
+
+type SmokeProps = {}
+
+export default class Smoke extends Component<SmokeProps> {
+   template: Template = [
+      {
+         component: new AnimatedSprite(),
+         position: (): Coordinates => new Coordinates(0, 0),
+         props: (): AnimatedSpriteProps => {
+            return {
+               width: 128,
+               height: 128,
+               spriteWidth: 16,
+               spriteHeight: 16,
+               interval: 120,
+               source: smokeSprites
+            }
+         }
+      }
+   ]
+}
