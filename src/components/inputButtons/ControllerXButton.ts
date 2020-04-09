53d0b52377303204f6726015490206d3338446a2
Felix Wotschofsky <hello@felisk.io>
2020-04-09 13:08:13 +0200

    Add controller X button component 🛎

---
+++ src/components/inputButtons/ControllerXButton.ts
@@ -0,0 +1,24 @@
+import { Template } from '../../../lib/Types';
+import Component from '../../../lib/Component';
+import Coordinates from '../../../lib/helpers/Coordinates';
+import PropsContext from '../../../lib/PropsContext';
+
+import BaseButton, { ButtonPropsTemplate, BaseButtonProps } from './BaseButton';
+import controllerXSprite from '../../assets/inputs/controller_x.png';
+import controllerXPressedSprite from '../../assets/inputs/controller_x_pressed.png';
+
+export type ControllerXButtonProps = ButtonPropsTemplate;
+
+export default class ControllerXButton extends Component<ControllerXButtonProps> {
+  template: Template = [
+    {
+      component: new BaseButton(),
+      position: (): Coordinates => new Coordinates(0, 0),
+      props: (ctx: PropsContext<ControllerXButtonProps>): BaseButtonProps => ({
+        pressed: ctx.props.pressed,
+        sprite: controllerXSprite,
+        spritePressed: controllerXPressedSprite
+      })
+    }
+  ];
+}
