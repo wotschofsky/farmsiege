1e95c3194290821dc07d1f00b2183c8909312d3e
Felix Wotschofsky <hello@felisk.io>
2020-04-10 16:56:51 +0200

    Removed unused code from Character 🐱‍💻

--- src/components/character/Character.ts
+++ src/components/character/Character.ts
@@ -11,10 +11,6 @@ import Shirt from './Shirt';
 import Pants from './Pants';
 import blackManLeftSprite from '../../assets/character/body/black_man_left.png';
 import blackManRightSprite from '../../assets/character/body/black_man_right.png';
-// import spriteIdleLeft from '../../assets/finn_idle_left.png';
-// import spriteIdleRight from '../../assets/finn_idle_right.png';
-// import spriteRunningLeft from '../../assets/finn_running_left.png';
-// import spriteRunningRight from '../../assets/finn_running_right.png';
 
 export type CharacterProps = {
   direction: Directions;
@@ -45,21 +41,5 @@ export default class Character extends Component<CharacterProps> {
       component: new Hat(),
       position: (): Coordinates => new Coordinates(0, -48 * values.character.size)
     }
-    // {
-    //    component: new AnimatedSprite(),
-    //    position: (): Coordinates => new Coordinates(
-    //       this.stores.character.content.posX - 64,
-    //       this.stores.character.content.posY - 128,
-    //    ),
-    //    props: (): AnimatedSpriteProps => ({
-    //       source: this.activeSprite,
-    //       // source: blackManSprite,
-    //       spriteWidth: 32,
-    //       spriteHeight: 32,
-    //       width: 256,
-    //       height: 256,
-    //       interval: 150
-    //    }),
-    // }
   ];
 }
