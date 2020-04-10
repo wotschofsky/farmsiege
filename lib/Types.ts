1e4d7c8a329c8a69497efa5bc88a644e9fc7e9c5
Felix Wotschofsky <hello@felisk.io>
2020-04-10 16:24:40 +0200

    Extract TransformationConfig type 🛠

--- lib/Types.ts
+++ lib/Types.ts
@@ -2,19 +2,19 @@ import Component from './Component';
 import Coordinates from './helpers/Coordinates';
 import PropsContext from './PropsContext';
 
+export type TransformationConfig = {
+  rotate?: {
+    center: Coordinates;
+    angle: number;
+  };
+};
+
 export type TemplateItem = {
   component: Component<any>;
   position: (context: PropsContext<any>) => Coordinates;
   props?: (context: PropsContext<any>) => { [key: string]: any };
   show?: (context?: PropsContext<any>) => boolean;
-  transform?: (
-    context: PropsContext<any>
-  ) => {
-    rotate?: {
-      center: Coordinates;
-      angle: number;
-    };
-  };
+  transform?: (context: PropsContext<any>) => TransformationConfig;
 };
 
 export type Template = TemplateItem[];
