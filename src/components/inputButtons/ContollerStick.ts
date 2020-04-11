a81fa64adfeda57880f844bf65159fe6ba0e8fed
Felix Wotschofsky <hello@felisk.io>
2020-04-11 16:27:32 +0200

    Add direction support to ControllerStick 🕹

--- src/components/inputButtons/ContollerStick.ts
+++ src/components/inputButtons/ContollerStick.ts
@@ -1,4 +1,4 @@
-import { Template } from '../../../lib/Types';
+import { Template, TransformationConfig } from '../../../lib/Types';
 import Component from '../../../lib/Component';
 import Coordinates from '../../../lib/helpers/Coordinates';
 import PropsContext from '../../../lib/PropsContext';
@@ -6,9 +6,11 @@ import PropsContext from '../../../lib/PropsContext';
 import BaseButton, { BaseButtonProps } from './BaseButton';
 import controllerStickLeftSprite from '../../assets/inputs/controller_stick_left.png';
 import controllerStickRightSprite from '../../assets/inputs/controller_stick_right.png';
+import { Directions } from '../../../lib/Enums';
 
 export type ControllerStickProps = {
   type: 'left' | 'right';
+  direction?: null | Directions;
 };
 
 export default class ControllerStick extends Component<ControllerStickProps> {
@@ -30,6 +32,24 @@ export default class ControllerStick extends Component<ControllerStickProps> {
           pressed: false,
           sprite: sprite,
           spritePressed: sprite
+        };
+      },
+      transform: (ctx: PropsContext<ControllerStickProps>): TransformationConfig => {
+        let angle = 0;
+        switch (ctx.props.direction) {
+          case Directions.Left:
+            angle = -0.05 * Math.PI;
+            break;
+          case Directions.Right:
+            angle = 0.05 * Math.PI;
+            break;
+        }
+
+        return {
+          rotate: {
+            angle: angle,
+            center: new Coordinates(32, 64)
+          }
         };
       }
     }
