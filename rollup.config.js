32de2088363a96e6ed4938f5dfbc0972b4652f6b
Felix Wotschofsky <hello@felisk.io>
2020-04-12 01:07:26 +0200

    Upgrade rollup plugins 🔌

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,12 @@
-// import typescript from 'rollup-plugin-typescript2'
 import { eslint } from 'rollup-plugin-eslint';
 import { terser } from 'rollup-plugin-terser';
 import babel from 'rollup-plugin-babel';
-import commonjs from 'rollup-plugin-commonjs';
+import commonjs from '@rollup/plugin-commonjs';
 import copy from 'rollup-plugin-copy';
 import htmlTemplate from 'rollup-plugin-generate-html-template';
 import json from '@rollup/plugin-json';
 import livereload from 'rollup-plugin-livereload';
-import nodeResolve from 'rollup-plugin-node-resolve';
+import resolve from '@rollup/plugin-node-resolve';
 import serve from 'rollup-plugin-serve';
 import typescript from 'rollup-plugin-typescript';
 import url from 'rollup-plugin-url';
@@ -42,7 +41,6 @@ export default {
     {
       file: 'dist/bundle.js',
       format: 'iife',
-      // sourcemap: 'inline'
       sourcemap: !process.env.PRODUCTION
     }
   ],
@@ -50,7 +48,6 @@ export default {
     url({
       limit: 100 * 1024, // inline files < 10k, copy files > 10k
       // limit: Infinity,
-      // include: ['**/*.svg'], // defaults to .svg, .png, .jpg and .gif files
       include: ['**/*.png', '**/*.mp3'],
       emitFiles: true, // defaults to true,
       fileName: 'assets/[dirname][hash][extname]'
@@ -59,13 +56,11 @@ export default {
     typescript({
       // clean: true
     }),
-    commonjs({}),
+    commonjs(),
     babel({
       // exclude: 'node_modules/**'
     }),
-    nodeResolve({
-      jsnext: true
-    }),
+    resolve(),
     htmlTemplate({
       template: 'src/index.html',
       target: 'dist/index.html'
