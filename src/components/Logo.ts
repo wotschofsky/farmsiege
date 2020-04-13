cabbb065142c47037022974065caebd7ea0fb084
Felix Wotschofsky <hello@felisk.io>
2020-04-13 23:36:27 +0200

    Add progress option to Logo 🕰

--- src/components/Logo.ts
+++ src/components/Logo.ts
@@ -4,10 +4,20 @@ import Sprite, { SpriteProps } from '../../lib/components/native/Sprite';
 import Coordinates from '../../lib/helpers/Coordinates';
 
 import logo from '../assets/logo.png';
+import PropsContext from '../../lib/PropsContext';
 
-export type LogoProps = {};
+export type LogoProps = {
+  progress?: number;
+};
 
 export default class Logo extends Component<LogoProps> {
+  private getPartsShown(progress: number): number {
+    if (progress >= 1 / 2) {
+      return 1;
+    }
+    return 0;
+  }
+
   protected template: Template = [
     {
       component: new Sprite(),
@@ -16,7 +26,12 @@ export default class Logo extends Component<LogoProps> {
         source: logo,
         width: 712,
         height: 296
-      })
+      }),
+      show: (ctx: PropsContext<LogoProps> | undefined): boolean => {
+        if (!ctx) return false;
+        if (ctx.props.progress === undefined) return true;
+        return this.getPartsShown(ctx.props.progress) >= 1;
+      }
     }
   ];
 }
