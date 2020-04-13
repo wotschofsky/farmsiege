686dbfb13b9112677ccfaf9d881bf4432b5e8350
Felix Wotschofsky <hello@felisk.io>
2020-04-14 01:04:20 +0200

    Center ScoreCounter 🎯

--- src/components/ScoreCounter.ts
+++ src/components/ScoreCounter.ts
@@ -11,21 +11,22 @@ export default class ScoreCounter extends Component<ScoreCounterProps> {
   protected template: Template = [
     {
       component: new Rectangle(),
-      position: (): Coordinates => new Coordinates(-50, 0),
+      position: (): Coordinates => new Coordinates(-75, 0),
       props: (): RectangleProps => ({
-        width: 100,
+        width: 150,
         height: 50,
         color: 'rgba(0, 0, 0, 0.5)'
       })
     },
     {
       component: new Text(),
-      position: (): Coordinates => new Coordinates(-30, 30),
+      position: (): Coordinates => new Coordinates(2, 30),
       props: (): TextProps => {
         const statsStore = <StatsStore>this.stores.score;
 
         return {
           text: `${statsStore.content.score} Points`,
+          align: 'center',
           color: '#fff',
           font: 'Heartbit',
           size: 32
