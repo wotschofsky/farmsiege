5853318e735738ca264f448a22834e647fd78e33
Felix Wotschofsky <hello@felisk.io>
2020-04-14 18:47:27 +0200

    Add visualize option to EventListener 🖼

--- lib/components/logical/EventListener.ts
+++ lib/components/logical/EventListener.ts
@@ -7,6 +7,7 @@ import Dimensions from '../../helpers/Dimensions';
 export type EventListenerProps = {
   size: Dimensions;
   onClick?: (position: Coordinates) => void;
+  visualize?: boolean;
 };
 
 export default class EventListener extends Component<EventListenerProps> {
@@ -37,5 +38,19 @@ export default class EventListener extends Component<EventListenerProps> {
     this.componentPosition = position;
     this.componentSize = props.size;
     this.renderContext = context;
+
+    if (props.visualize) {
+      context.renderContext.beginPath();
+      context.renderContext.rect(
+        (position.x + context.parentX) * context.scaleFactor,
+        (position.y + context.parentY) * context.scaleFactor,
+        props.size.width * context.scaleFactor,
+        props.size.height * context.scaleFactor
+      );
+
+      context.renderContext.lineWidth = 5;
+      context.renderContext.strokeStyle = '#0f0';
+      context.renderContext.stroke();
+    }
   }
 }
