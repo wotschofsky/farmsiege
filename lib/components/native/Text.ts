7b66c8f83a5f5214fade9a202d8cbaa8427b2e4f
Felix Wotschofsky <hello@felisk.io>
2020-04-14 18:46:47 +0200

    Add baseline option to Text 📏

--- lib/components/native/Text.ts
+++ lib/components/native/Text.ts
@@ -8,11 +8,13 @@ export type TextProps = {
   font?: string;
   size?: number;
   align?: CanvasTextAlign;
+  baseline?: CanvasTextBaseline;
 };
 
 export default class Text extends Component<TextProps> {
   public render(context: RenderingContext, position: Coordinates, props: TextProps): void {
     context.renderContext.textAlign = props.align || 'start';
+    context.renderContext.textBaseline = props.baseline || 'alphabetic';
     context.renderContext.fillStyle = props.color || 'black';
     context.renderContext.font = `${(props.size || 16) * context.scaleFactor}px ${props.font || 'Arial'}`;
     context.renderContext.fillText(
