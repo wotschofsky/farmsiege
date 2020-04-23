f31d040af680362c02521b2c8ecb02069ea89e8e
Felix Wotschofsky <hello@felisk.io>
2020-04-23 16:09:58 +0200

    Fix backend not being authenticated ✔

--- api/submitScore.ts
+++ api/submitScore.ts
@@ -3,10 +3,20 @@ import qs from 'querystring';
 import fetch from 'node-fetch';
 import { NowRequest, NowResponse } from '@now/node';
 
-import serviceAccount from './firebase-account.json';
-
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    type: 'service_account',
+    project_id: 'garden-defense',
+    private_key_id: '5bf7c2ee19ba6f5a06bfb903d79e98a03e138d6e',
+    private_key: process.env.FIREBASE_PRIVATE_KEY,
+    client_email: 'firebase-adminsdk-afrg3@garden-defense.iam.gserviceaccount.com',
+    client_id: '117021851025152874515',
+    auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+    token_uri: 'https://oauth2.googleapis.com/token',
+    auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+    client_x509_cert_url:
+      'https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-afrg3%40garden-defense.iam.gserviceaccount.com'
+  }),
   databaseURL: 'https://garden-defense.firebaseio.com'
 });
 const db = admin.database();
