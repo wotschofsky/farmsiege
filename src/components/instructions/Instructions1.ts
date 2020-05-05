e92184034f51755358df81ad648f2c91aa9b0ffe
Felix Wotschofsky <hello@felisk.io>
2020-05-06 00:05:22 +0200

    Fix typo in filename ✔

--- src/components/instructions/Instructions1.ts
+++ src/components/instructions/Instructions1.ts
@@ -7,7 +7,7 @@ import PropsContext from '../../../lib/PropsContext';
 
 import { HoldableItems } from '../../store/CharacterStore';
 import ArrowButtons, { ArrowButtonsProps } from '../inputButtons/KeyboardArrowButtons';
-import ControllerStick, { ControllerStickProps } from '../inputButtons/ContollerStick';
+import ControllerStick, { ControllerStickProps } from '../inputButtons/ControllerStick';
 import WASDButtons, { WASDButtonsProps } from '../inputButtons/KeyboardWASDButtons';
 
 export type Instructions1Props = {};
